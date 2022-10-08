279e5bb1e74a031fc801d975b8aa7607f71bdb86
cima22 <gab.cima22@gmail.com>
2022-10-08 19:29:31 +0200

    exercises

--- assignment_1.java
+++ assignment_1.java
@@ -1,5 +1,25 @@
 public class Hello{
 
-	public static void main (String[] args){
-	}
+ public static void main (String[] args){
+
+ int len = args.length;
+ System.out.print("Hello ");
+
+ switch(len){
+	 case 0:
+	 	System.out.println("everybody!");
+	 	break;
+	
+	case 1:
+		System.out.println(args[0] + "!");
+		break;
+	
+	default:
+		for(int i = 0; i < len; i++){
+			String space = i == len - 1 ? "!" : i == len - 2 ? " and, " : ", ";
+			System.out.print(args[i] + space);
+		}
+ }
+   
+ }
 }
--- assignment_2.java
+++ assignment_2.java
@@ -1,5 +1,54 @@
+import java.lang.Math;
 public class Calculator{
 
-	public static void main (String[] args){
-	}
+ public static void main (String[] args){
+ 
+  int len = args.length;
+  float res, op2;
+
+  if(len < 3){
+   System.out.println("Bruh");
+   return;
+  }
+
+ res = Float.parseFloat(args[0]);
+
+ for(int i = 1; i < len; i += 2){
+
+   try{
+	op2 = Float.parseFloat(args[i + 1]);
+   }
+
+   catch(NumberFormatException e){
+    System.out.println("Jesus Marie they're numbers not strings");
+    return;
+   }
+
+   switch(args[i]){
+	   case "+":
+		   res += op2;
+		   System.out.println(res);
+		   break;
+	   case "-":
+		   res -= op2;
+		   System.out.println(res);
+		   break;
+	   case "*":
+		   res *= op2;
+		   System.out.println(res);
+		   break;
+	   case "/":
+		   if(Math.abs(op2) < 0.0000001){
+			System.out.println("Ayo mr White!");
+			return;
+		   }
+		   res /= op2;
+		   System.out.println(res);
+		   break;
+	default:
+		   System.out.println("It is ... acceptable.");
+   }
+ }
+   return;
+ }
 }
